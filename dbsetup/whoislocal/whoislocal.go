aaff39d3f9b4968f24d8af429a54e4d0c27f0855
SSolanoB <s.solano586@uniandes.edu.co>
2020-06-29 08:28:58 -0500

    deletes comments in import, adds new comments in examples of ip addresses, and modifies return statement

--- dbsetup/whoislocal/whoislocal.go
+++ dbsetup/whoislocal/whoislocal.go
@@ -1,17 +1,16 @@
 package whoislocal
 
 import (
-	//"log"
 	"fmt"
 	"github.com/likexian/whois-go"
-	//"github.com/likexian/whois-parser-go"
+	
 	"regexp"
 	"strings"
 )
 
 func AskforIp(query string) (country, org_name string, err error){
-	//query := "2607:f8b0:4005:808:0:0:0:200e"
-	//query = "205.251.242.103"
+	//query := "2607:f8b0:4005:808:0:0:0:200e" ipv6
+	//query = "205.251.242.103" ipv4
 	request, err := whois.Whois(query)
 	if err == nil {
 		fmt.Println(request)
@@ -23,9 +22,11 @@ func AskforIp(query string) (country, org_name string, err error){
 		re_2 := regexp.MustCompile(`(?:OrgName:) (.*)`)
 		org_name := re_2.FindAllStringSubmatch(string(request), -1)[0][1]
 		fmt.Printf("%q\n", strings.TrimSpace(org_name))
-		return
+		return country, org_name, err
 	} else {
-		return "", "", err
+		var country string
+		var org_name string
+		return country, org_name, err
 	}
 
 }
