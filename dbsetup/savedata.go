04a79e1cd49b97db4364d9d0b301d5b676115b16
SSolanoB <s.solano586@uniandes.edu.co>
2020-07-01 01:04:35 -0500

    adds function to check if servers have changed

--- dbsetup/savedata.go
+++ dbsetup/savedata.go
@@ -230,20 +230,126 @@ func checkPreviousGrade(tx *sql.Tx, last_inquiry_id *int) (previous_ssl_grade *s
     log.Printf("Nothing in here")
   }
 
-
-  /*var inq_ids []int
-
-  for rows_in.Next() {
-    var id int
-    if err := rows_in.Scan(&id); err != nil {
-      return err
-    }
-    inq_ids = append(inq_ids, id)
-  }
-
-  fmt.Println("CHECK PREVIOUS INQUIRIES")
-  fmt.Println(inq_ids)*/
-
-
   return previous_ssl_grade, nil
 }
+
+func checkServersChanged(tx *sql.Tx, last_inquiry_id *int, current_inquiry_id *int) (servers_changed *bool, error error) {
+  p_true := true
+
+  rows, error := tx.Query("SELECT inquiry_id, address, ssl_grade, country, owner FROM servers WHERE inquiry_id = $1 OR inquiry_id = $2 ORDER BY address", last_inquiry_id, current_inquiry_id)
+  defer rows.Close()
+
+  switch {
+  case error == sql.ErrNoRows:
+    log.Println("no servers\n")
+    return nil, nil
+  case error != nil:
+    log.Fatalf("query error: %v\n", error)
+    return nil, nil
+  default:
+    log.Printf("Nothing in here")
+  }
+
+  var b_addresses, b_ssl_grades, b_countries, b_owners []string
+  var c_addresses, c_ssl_grades, c_countries, c_owners []string
+
+  for rows.Next() {
+    var inquiry_id, address, ssl_grade, country, owner string
+
+    if err := rows.Scan(&inquiry_id, &address, &ssl_grade, &country, &owner); err != nil {
+      return nil, err
+    }
+
+    if inquiry_id == strconv.Itoa(*last_inquiry_id) {
+      b_addresses = append(b_addresses, address)
+      b_ssl_grades = append(b_ssl_grades, ssl_grade)
+      b_countries = append(b_countries, country)
+      b_owners = append(b_owners, owner)
+    } else {
+      c_addresses = append(c_addresses, address)
+      c_ssl_grades = append(c_ssl_grades, ssl_grade)
+      c_countries = append(c_countries, country)
+      c_owners = append(c_owners, owner)
+    }
+  }
+
+  fmt.Println("CHECK PREVIOUS SERVERS")
+  fmt.Println(b_addresses)
+  fmt.Println(b_ssl_grades)
+  fmt.Println(b_countries)
+  fmt.Println(b_owners)
+
+  fmt.Println("CHECK CURRENT SERVERS")
+  fmt.Println(c_addresses)
+  fmt.Println(c_ssl_grades)
+  fmt.Println(c_countries)
+  fmt.Println(c_owners)
+
+  /*before_rows, error_bef := tx.Query("SELECT address, ssl_grade, country, owner FROM servers WHERE inquiry_id = $1 ORDER BY address", last_inquiry_id)
+  defer before_rows.Close()
+  current_rows, error_cur := tx.Query("SELECT address, ssl_grade, country, owner FROM servers WHERE inquiry_id = $1 ORDER BY address", current_inquiry_id)
+  defer current_rows.Close()*/
+
+  /*switch {
+  case error_bef == sql.ErrNoRows && error_cur == sql.ErrNoRows:
+    log.Println("no servers for any inquiry\n")
+    return nil, nil
+  case error_bef == sql.ErrNoRows && error_cur == nil:
+    log.Println("no servers in last inquiry, but in new one is ok\n")
+    return &p_true, nil
+  case error_cur == sql.ErrNoRows && error_bef == nil:
+    log.Println("no servers in current inquiry, but in last one is ok\n")
+    return &p_true, nil
+  case error_bef != nil || error_cur != nil:
+    if error_bef != nil {
+      log.Fatalf("query error: %v\n", error_bef)
+    }
+    if error_cur != nil {
+      log.Fatalf("query error: %v\n", error_cur)
+    }
+    return 
+  default:
+    log.Printf("Nothing in here")
+  }*/
+
+  /*var b_addresses, b_ssl_grades, b_countries, b_owners []string
+
+  for before_rows.Next() {
+    var address, ssl_grade, country, owner string
+    if err := before_rows.Scan(&address, &ssl_grade, &country, &owner); err != nil {
+      return nil, err
+    }
+    b_addresses = append(b_addresses, address)
+    b_ssl_grades = append(b_ssl_grades, ssl_grade)
+    b_countries = append(b_countries, country)
+    b_owners = append(b_owners, owner)
+  }
+
+  fmt.Println("CHECK PREVIOUS SERVERS")
+  fmt.Println(b_addresses)
+  fmt.Println(b_ssl_grades)
+  fmt.Println(b_countries)
+  fmt.Println(b_owners)
+
+  var c_addresses, c_ssl_grades, c_countries, c_owners []string
+
+  for current_rows.Next() {
+    var address, ssl_grade, country, owner string
+    if err := current_rows.Scan(&address, &ssl_grade, &country, &owner); err != nil {
+      return nil, err
+    }
+    c_addresses = append(c_addresses, address)
+    c_ssl_grades = append(c_ssl_grades, ssl_grade)
+    c_countries = append(c_countries, country)
+    c_owners = append(c_owners, owner)
+  }
+
+  fmt.Println("CHECK CURRENT SERVERS")
+  fmt.Println(c_addresses)
+  fmt.Println(c_ssl_grades)
+  fmt.Println(c_countries)
+  fmt.Println(c_owners)*/
+
+  return &p_true, nil
+}
+
