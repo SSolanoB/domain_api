aba35273830596e7a81055f59bdac8ab402344d3
SSolanoB <s.solano586@uniandes.edu.co>
2020-06-30 23:17:36 -0500

    adds check previous servers function into savedata file

--- dbsetup/savedata.go
+++ dbsetup/savedata.go
@@ -151,14 +151,77 @@ func SaveData(tx *sql.Tx, resp ssllabsapi.Response) error {
     fmt.Printf("Nor url provided")
   }
 
-  // Perform the transfer.
-  /*if _, err := tx.Exec(
-    "UPDATE accounts SET balance = balance - $1 WHERE id = $2", amount, from); err != nil {
-    return err
-  }
-  if _, err := tx.Exec(
-    "UPDATE accounts SET balance = balance + $1 WHERE id = $2", amount, to); err != nil {
-    return err
-  }*/
   return nil
+}
+
+func checkPreviousServers(tx *sql.Tx, url string) (previous_ssl_grade *string, error error) {
+  
+  trimmed_url := strings.Trim(url, "http://")
+  trimmed_url = strings.Trim(trimmed_url, "https://")
+
+  rows, error := tx.Query("SELECT id FROM domains WHERE url LIKE $1", "%" + trimmed_url + "%")
+
+  if error != nil {
+    return
+  }
+  defer rows.Close()
+
+  var dom_ids []int
+
+  for rows.Next() {
+    var id int
+    if error = rows.Scan(&id); error != nil {
+      return
+    }
+    dom_ids = append(dom_ids, id)
+  }
+
+  fmt.Println("CHECK PREVIOUS DOMAINS 2")
+  fmt.Println(dom_ids)
+
+  var last_inquiry_id *int
+  error = tx.QueryRow("SELECT inquiries.id FROM inquiries INNER JOIN domains ON domains.id = inquiries.domain_id AND domains.id = ANY ($1) WHERE inquiries.created_at < (now() - INTERVAL '1 HOUR') ORDER BY inquiries.created_at DESC LIMIT 1", pq.Array(dom_ids)).Scan(&last_inquiry_id)
+
+  switch {
+  case error == sql.ErrNoRows:
+    log.Printf("no inquiries found with domain id in %d\n", dom_ids)
+    return nil, nil
+  case error != nil:
+    log.Fatalf("query error: %v\n", error)
+    return 
+  default:
+    log.Printf("Nothing in here")
+  }
+  
+  fmt.Printf("Inquiry id is: %+v\n", *last_inquiry_id)
+
+  error = tx.QueryRow("SELECT ssl_grade FROM inquiries WHERE id = $1", last_inquiry_id).Scan(&previous_ssl_grade)
+
+  switch {
+  case error == sql.ErrNoRows:
+    log.Printf("no ssl_grade found with inquiry id %d\n", last_inquiry_id)
+    return nil, nil
+  case error != nil:
+    log.Fatalf("query error: %v\n", error)
+    return 
+  default:
+    log.Printf("Nothing in here")
+  }
+
+
+  /*var inq_ids []int
+
+  for rows_in.Next() {
+    var id int
+    if err := rows_in.Scan(&id); err != nil {
+      return err
+    }
+    inq_ids = append(inq_ids, id)
+  }
+
+  fmt.Println("CHECK PREVIOUS INQUIRIES")
+  fmt.Println(inq_ids)*/
+
+
+  return previous_ssl_grade, nil
 }
